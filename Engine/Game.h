e8ae396e10f0f1c020ef932b5e5589c3c744b177
TheUnicum <tia_ben@tin.it>
2019-12-03 15:29:00 +0100

    [T08] - [Poo Game/RNG] Tutorial 8.

--- Engine/Game.h
+++ Engine/Game.h
@@ -36,11 +36,35 @@ private:
 	void UpdateModel();
 	/********************************/
 	/*  User Functions              */
+	void DrawFace(int x, int y);
+	void DrawPoo(int x, int y);
+	void DrawGameOver(int x, int y);
+	void DrawTileScreen(int x, int y);
+	int ClampScreenX(int x, int width);
+	int ClampScreenY(int y, int height);
+	bool IsColliding(int x0, int y0, int width0, int height0,
+		int x1, int y1, int width1, int height1);
 	/********************************/
 private:
 	MainWindow& wnd;
 	Graphics gfx;
 	/********************************/
 	/*  User Variables              */
+	int dudeX = 400;
+	int dudeY = 300;
+	int dudeWidth = 20;
+	int dudeHeight = 20;
+	int poo0X = 300;
+	int poo0Y = 150;
+	bool poo0IsEaten = false;
+	int poo1X = 100;
+	int poo1Y = 500;
+	bool poo1IsEaten = false;
+	int poo2X = 600;
+	int poo2Y = 400;
+	bool poo2IsEaten = false;
+	int pooWidth = 24;
+	int pooHeight = 24;
+	bool isStarted = false;
 	/********************************/
 };

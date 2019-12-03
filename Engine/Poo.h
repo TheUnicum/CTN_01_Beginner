ab6aca3d7f6b2e1b31b8615624d2fbb82fe1e9e6
TheUnicum <tia_ben@tin.it>
2019-12-03 21:09:44 +0100

    [T12] - [Constructor / Encapsulation] Tutorial 12.

--- Engine/Poo.h
+++ Engine/Poo.h
@@ -6,9 +6,12 @@
 class Poo
 {
 public:
+	Poo(int in_x, int in_y, int in_vx, int in_vy);
 	void Update();
 	void ProcessConsumprion(const Dude& dude);
 	void Draw(Graphics& gfx) const;
+	bool IsEaten() const;
+private:
 	int x;
 	int y;
 	int vx;

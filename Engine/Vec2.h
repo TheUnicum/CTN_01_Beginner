376e74524c8da200cb0ed5ad641d49039808c4e3
TheUnicum <tia_ben@tin.it>
2019-12-06 16:51:41 +0100

    [T19] - [2D Vectors] Tutorial 19

--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -9,6 +9,12 @@ public:
 	Vec2& operator+=(const Vec2& rhs);
 	Vec2 operator*(float rhs) const;
 	Vec2& operator*=(float rhs);
+	Vec2 operator-(const Vec2& rhs) const;
+	Vec2& operator-=(const Vec2& rhs);
+	float GetLenght() const;
+	float GetLenghtSq() const;
+	Vec2& Normalized();
+	Vec2 GetNormalized();
 public:
 	float x;
 	float y;

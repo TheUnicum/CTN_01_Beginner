6d256c8829dc47dca8df9c897be2fe58164eddf7
TheUnicum <tia_ben@tin.it>
2019-12-07 18:48:27 +0100

    <squashed it with SCIENCE>

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -1,5 +1,6 @@
 #include "Brick.h"
 #include <assert.h>
+#include <cmath>
 
 Brick::Brick(const RectF& rect_in, Color color_in)
 	:
@@ -27,7 +28,11 @@ void Brick::ExecuteBallCollision(Ball& ball)
 	assert(CheckBallCollision(ball));
 
 	const Vec2 ballPos = ball.GetPosition();
-	if (ballPos.x > rect.left && ballPos.x < rect.right)
+	if (std::signbit(ball.GetVelocity().x) == std::signbit((ballPos - GetCenter()).x))
+	{
+		ball.ReboundY();
+	}
+	else if (ballPos.x > rect.left && ballPos.x < rect.right)
 	{
 		ball.ReboundY();
 	}
